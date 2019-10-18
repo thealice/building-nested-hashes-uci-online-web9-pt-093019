c167454fc98311ab639c1e4db71e9199bd10ab64
Alice Freda <alicefreda@gmail.com>
2019-10-18 21:52:46 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -7,7 +7,10 @@ def first_challenge
       :hero_friends => []
     },
     :capulet => {
-
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
     }
   }
 end
