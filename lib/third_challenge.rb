311c510bfe71fc60d3cadacc32b9365e68a71d52
Alice Freda <alicefreda@gmail.com>
2019-10-18 21:59:25 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -6,22 +6,29 @@ def third_challenge
         :age => "53"
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => "Lady Montague",
+        :age => "54"
         },
       :hero => {
-        #your key/value pairs here
+        :name => "Romeo",
+        :age => "15",
+        :status => "alive"
         },
       :hero_friends => []
    },
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        :name => "Lord Montague",
+        :age => "53"
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => "Lord Montague",
+        :age => "53"
         },
       :heroine => {
-        #your key/value pairs here
+        :name => "Lord Montague",
+        :age => "53",
+        :status => "alive"
         },
       :heroine_friends => []
    }
